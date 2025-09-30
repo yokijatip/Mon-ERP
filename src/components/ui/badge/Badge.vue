fd67cf217081ce0761b82976a50516f82eca1dcd
Yoki Jati Perkasa <112103702+yokijatip@users.noreply.github.com>
2025-09-30 12:36:07 +0700

    Add Complete Onboarding View

---
+++ src/components/ui/badge/Badge.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue"
+import type { BadgeVariants } from "."
+import { cn } from "@/lib/utils"
+import { badgeVariants } from "."
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"]
+  variant?: BadgeVariants["variant"]
+}>()
+</script>
+
+<template>
+  <div
+    data-slot="badge"
+    :class="cn(badgeVariants({ variant }), props.class)"
+  >
+    <slot />
+  </div>
+</template>
