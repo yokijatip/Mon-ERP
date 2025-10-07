ee188423a4b0f28e98f74c1e50db6b20312ee930
Yoki Jati Perkasa <yokijati@gmail.com>
2025-10-07 23:52:56 +0700

    initial Home Dashboard

--- src/views/home/HomeDashboard.vue
+++ src/views/home/HomeDashboard.vue
@@ -1,24 +1,38 @@
 <script setup lang="ts">
+import {useAuth} from "@/composables/useAuth.ts";
+
+const { user } = useAuth()
+
 </script>
 
 <template>
   <div class="space-y-4">
-    <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-      <div class="aspect-video rounded-xl bg-muted/50 flex items-center justify-center">
-        <p class="text-muted-foreground">Chart 1</p>
-      </div>
-      <div class="aspect-video rounded-xl bg-muted/50 flex items-center justify-center">
-        <p class="text-muted-foreground">Chart 2</p>
-      </div>
-      <div class="aspect-video rounded-xl bg-muted/50 flex items-center justify-center">
-        <p class="text-muted-foreground">Chart 3</p>
-      </div>
+
+
+<!--  Title or Header Start  -->
+    <div>
+      <h1>Welcome, <span class="font-bold">{{ user?.name | "Guest" }}</span></h1>
     </div>
-    <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min flex items-center justify-center">
-      <div class="text-center">
-        <h1 class="text-2xl font-bold mb-2">Home Dashboard</h1>
-        <p class="text-muted-foreground">Welcome to your ERP dashboard</p>
-      </div>
-    </div>
+<!--  Title or Hedaer End  -->
+
+<!--  TEMPLATE DARI SHAdCN VUE  -->
+
+<!--    <div class="grid auto-rows-min gap-4 md:grid-cols-3">-->
+<!--      <div class="aspect-video rounded-xl bg-muted/50 flex items-center justify-center">-->
+<!--        <p class="text-muted-foreground">Chart 1</p>-->
+<!--      </div>-->
+<!--      <div class="aspect-video rounded-xl bg-muted/50 flex items-center justify-center">-->
+<!--        <p class="text-muted-foreground">Chart 2</p>-->
+<!--      </div>-->
+<!--      <div class="aspect-video rounded-xl bg-muted/50 flex items-center justify-center">-->
+<!--        <p class="text-muted-foreground">Chart 3</p>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min flex items-center justify-center">-->
+<!--      <div class="text-center">-->
+<!--        <h1 class="text-2xl font-bold mb-2">Home Dashboard</h1>-->
+<!--        <p class="text-muted-foreground">Welcome to your ERP dashboard</p>-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
